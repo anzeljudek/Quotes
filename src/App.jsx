d829d5481bab424d20d1f5e7e8756052be2c2419
anzeljudek <anze.hribar@outlook.com>
2024-02-28 18:01:18 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { Quote } from "Quote.jsx";
+import Quote from "./Quote";
+import { Badge } from "@/components/ui/badge";
 
 export default function App() {
   const [quotes, setQuotes] = useState([]);
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3", "tag4"]);
 
   async function getQuotes() {
     const request = await fetch("/quotes.json");
-    const podatki = request.json();
+    const podatki = await request.json();
 
     setQuotes(podatki);
   }
@@ -20,5 +22,15 @@ export default function App() {
     console.log(quotes);
   }, [quotes]);
 
-  return <>{quotes.length && <Quote quote={quotes[0]}>}</>;
+  return (
+    <>
+      <Badge variant="outline">Badge</Badge>
+
+      <div className="grid grid-cols-4 gap-4">
+        {quotes.map((quote) => (
+          <Quote quote={quote}></Quote>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,8 +1,12 @@
-export default function Quote(props);{
+export default function Quote(props) {
   return (
     <>
-    <div></div>
-
+      <div className="rounded-sm border-2 border-red-600 p-3 font-medium">
+        <p>{props.quote.content}</p>
+        <div className="font-bold">
+          <p>{props.quote.author}</p>
+        </div>
+      </div>
     </>
   );
 }
